31910b761396ee4860c2f47815001587d7712f12
Vifleem <sobole_80@mail.ru>
2020-03-12 22:20:01 +0600

    Updates

---
+++ precache-manifest.5b7d80de62a967c2d92c2bf170814620.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7272b6ae7d495a9ad8b9b5e0f4675ba2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8fb71ff8b4febe17f3cd",
+    "url": "./static/css/2.733ba2d0.chunk.css"
+  },
+  {
+    "revision": "0542f3234570bc58f07d",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "8fb71ff8b4febe17f3cd",
+    "url": "./static/js/2.7cf43a6a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "./static/js/2.7cf43a6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0542f3234570bc58f07d",
+    "url": "./static/js/main.7736cd27.chunk.js"
+  },
+  {
+    "revision": "924f1ed2fcb4d5f13259",
+    "url": "./static/js/runtime-main.4f8e16d7.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
